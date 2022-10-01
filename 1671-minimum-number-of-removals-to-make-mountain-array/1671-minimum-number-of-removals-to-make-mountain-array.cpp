365ebdb25b6bc38463f229286a771563bfd5060b
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-01 14:51:20 +0530

    Time: 488 ms (40.55%), Space: 12.1 MB (86.13%) - LeetHub

---
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    int minimumMountainRemovals(vector<int>& nums) {
+        vector<int> lis(nums.size() , 0);
+        lis[0] = 1;
+        for(int i = 1 ; i < nums.size() ; i++){
+            int maxim = 0;
+            for(int j = 0 ; j < i ; j++){
+                if(nums[i] > nums[j]){
+                    maxim = max(lis[j] , maxim);
+                }
+            }
+            lis[i] = maxim + 1;
+        }
+        
+        vector<int> lds(nums.size() , 0);
+        lds[nums.size() - 1] = 1;
+        for(int i = nums.size() - 2 ; i >= 0 ; i--){
+            int maxim = 0;
+            for(int j = i + 1 ; j < nums.size() ; j++){
+                if(nums[i] > nums[j]){
+                    maxim = max(lds[j] , maxim);
+                }
+            }
+            lds[i] = maxim + 1;
+        }
+        
+        int largestSum = INT_MIN;
+        for(int i = 0 ; i < nums.size() ; i++){
+            if(lds[i] == 1 || lis[i] == 1) continue;
+            largestSum = max(largestSum , (lds[i] + lis[i] - 1));
+        }
+        return nums.size() - largestSum;
+        
+    }
+};
