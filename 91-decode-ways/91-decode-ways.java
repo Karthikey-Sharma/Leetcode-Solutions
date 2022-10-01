b0daef4c264091794879d6cee4d2d36187172241
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-01 23:50:45 +0530

    Time: 21 ms (5.11%), Space: 40.4 MB (97.75%) - LeetHub

---
+++ 91-decode-ways/91-decode-ways.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int memo(String s ,int idx , int[] dp){
+        if(idx == s.length()){
+            return 1;
+        }
+        if(dp[idx] != 0) return dp[idx];
+        int ans = 0;
+        if(s.charAt(idx) >= '1' && s.charAt(idx) <= '9'){
+            ans += memo(s  , idx + 1 ,dp);
+        }
+        
+        if(idx + 1 < s.length() && (s.charAt(idx) == '1' || s.charAt(idx) == '2' && s.charAt(idx + 1) <= '6')){
+            ans += memo(s , idx + 2 ,dp );
+        }
+        return dp[idx] = ans;
+    }
+    
+    public int numDecodings(String s) {
+        int[] dp = new int[s.length() + 1];
+        return memo(s , 0 ,dp);
+    }
+}
