e1f926c5bc10a68cafdc0cb9de219a0adfaa144a
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-10 02:58:20 +0530

    Time: 319 ms (10.87%), Space: 45.2 MB (29.30%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,20 +1,5 @@
 class Solution {
 public:
-    int rec(int idx , vector<int>&coins , int amount , vector<vector<int>> &dp){
-        if(idx == 0){
-            if(amount % coins[idx] == 0) return amount / coins[idx];
-            else return 1e9;
-        }
-        if(dp[idx][amount] != -1){
-            return dp[idx][amount];
-        }
-        int take = INT_MAX;
-        if(coins[idx] <= amount){
-            take = 1 + rec(idx , coins , amount - coins[idx] ,dp);
-        }
-        int notTake = 0 + rec(idx - 1 , coins , amount ,dp );
-        return dp[idx][amount] = min(take , notTake);
-    }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp(coins.size() , vector<int>(amount + 1 , 0));
         for(int amt = 0 ; amt <= amount ; amt++){
