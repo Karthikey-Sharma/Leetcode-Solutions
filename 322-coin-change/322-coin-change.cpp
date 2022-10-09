73ef446932add6adf214ad8cfcbbb9ba43f52f07
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-10 02:58:05 +0530

    Time: 307 ms (12.23%), Space: 45.2 MB (26.50%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -16,12 +16,23 @@ public:
         return dp[idx][amount] = min(take , notTake);
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<vector<int>> dp(coins.size() , vector<int>(amount + 1 , -1));
-        int ans = rec(coins.size() - 1, coins , amount , dp);
-        if(ans >= 1e9){
-            return -1;
-        }else{
-            return ans;
+        vector<vector<int>> dp(coins.size() , vector<int>(amount + 1 , 0));
+        for(int amt = 0 ; amt <= amount ; amt++){
+             if(amt % coins[0] == 0) dp[0][amt] = amt / coins[0];
+             else dp[0][amt] = 1e9;
         }
+        for(int idx = 1 ; idx < coins.size() ; idx++){
+            for(int amt = 0 ; amt <= amount ; amt++){
+                int take = INT_MAX;
+                if(coins[idx] <= amt){
+                    take = 1 + dp[idx][amt - coins[idx]];
+                }
+                int notTake = 0 + dp[idx - 1][amt];
+                dp[idx][amt] = min(take , notTake);
+            }
+        }
+        int ans = dp[coins.size() - 1][amount];
+        if(ans >= 1e9) return -1;
+        else return ans;
     }
 };
