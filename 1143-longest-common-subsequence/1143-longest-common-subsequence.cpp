efe59b64bb7a61fc91bbabb168c3e562aa7f7572
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-27 02:51:24 +0530

    Time: 36 ms (60.26%), Space: 6.3 MB (99.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,19 +1,22 @@
 class Solution {
 public:
     int longestCommonSubsequence(string s, string t) {
+        // Space Optimixation o(m)
         int n = s.size();
         int m = t.size();
-        vector<vector<int>> dp(n + 1  , vector<int>(m + 1 , 0));
+        vector<int> prev(m + 1 , 0);
+        vector<int> curr(m + 1, 0);
         for(int i = 0 ; i <= n ; i++){
             for(int j = 0 ; j <= m ; j++){
                 if(i == 0 || j == 0){
-                    dp[i][j] = 0 ;
+                    prev[j] = 0 ;
                     continue;
                 }
-                if(s[i - 1] == t[j - 1])  dp[i][j] = 1 + dp[i - 1][j - 1];
-                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                if(s[i - 1] == t[j - 1]) curr[j] = 1 + prev[j - 1];
+                else curr[j] = max(prev[j], curr[j - 1]);
             }
+            prev = curr;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
