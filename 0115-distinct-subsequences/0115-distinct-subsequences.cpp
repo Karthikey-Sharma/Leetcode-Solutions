d96867b683c5c312add6a65b0f58471ed2e791e3
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-28 05:19:41 +0530

    Time: 72 ms (47.21%), Space: 6.4 MB (88.54%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -3,24 +3,28 @@ public:
     
     int numDistinct(string s, string t) {
         // shifting 1 index to handle negative base case for tabulation
-        vector<vector<double>> dp(s.size() + 1 , vector<double>(t.size() + 1 , 0));
+        // space optimization
+        
+        vector<double> prev(t.size() + 1);
+        vector<double> curr(t.size() + 1);
         for(int i = 0 ; i <= s.size() ; i++){
             for(int j = 0 ; j <= t.size() ; j++){
                 if(j == 0){
-                    dp[i][j] =  1;
+                    prev[j] =  1;
                     continue;
                 }
                 if(i == 0){
-                    dp[i][j] =  0;
+                    prev[j] =  0;
                     continue;
                 } 
                 
                 if(s[i - 1] == t[j - 1]){
-                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                    curr[j] = prev[j - 1] + prev[j];
                 }
-                else dp[i][j] =  dp[i - 1][j];
+                else curr[j] =  prev[j];
             }
+            prev = curr;
         }
-        return (int)dp[s.size()][t.size()];
+        return (int)prev[t.size()];
     }
 };
