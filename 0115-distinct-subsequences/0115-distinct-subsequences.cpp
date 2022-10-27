d0d0e46426c148ae8b5f23d334fe1771a31df2ac
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-28 05:17:48 +0530

    Time: 102 ms (20.46%), Space: 30.2 MB (6.61%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int rec(string &s , string &t , int i , int j , vector<vector<int>> &dp){
-        if(j == 0) return 1;
-        if(i == 0) return 0;
-        if(dp[i][j] != -1) return dp[i][j];
-        if(s[i - 1] == t[j - 1]){
-            return dp[i][j] = rec(s , t , i - 1 , j - 1 , dp) + rec(s , t , i - 1 , j , dp);
-        }
-        else return dp[i][j] = rec(s , t , i - 1 , j , dp);
-    }
+    
     int numDistinct(string s, string t) {
         // shifting 1 index to handle negative base case for tabulation
-        vector<vector<int>> dp(s.size() + 1 , vector<int>(t.size() + 1 , -1));
-        return rec(s , t , s.size()  , t.size() , dp);
+        vector<vector<double>> dp(s.size() + 1 , vector<double>(t.size() + 1 , 0));
+        for(int i = 0 ; i <= s.size() ; i++){
+            for(int j = 0 ; j <= t.size() ; j++){
+                if(j == 0){
+                    dp[i][j] =  1;
+                    continue;
+                }
+                if(i == 0){
+                    dp[i][j] =  0;
+                    continue;
+                } 
+                
+                if(s[i - 1] == t[j - 1]){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }
+                else dp[i][j] =  dp[i - 1][j];
+            }
+        }
+        return (int)dp[s.size()][t.size()];
     }
 };
