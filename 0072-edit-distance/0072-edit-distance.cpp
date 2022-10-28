c954ed01562cf4992ecf867dcd15f7f44587239d
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-28 19:37:26 +0530

    Time: 11 ms (89.56%), Space: 6.4 MB (97.68%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -3,24 +3,20 @@ public:
     int minDistance(string word1, string word2) {
         // doing index shifting for tabulation
         // Now Tabulation
-        vector<vector<int>> dp(word1.size() + 1 , vector<int>(word2.size() + 1 , 0));
-        for(int i = 0 ; i < dp.size() ; i++){
-            for(int j = 0 ; j < dp[i].size() ; j++){
-                if(j == 0){
-                    dp[i][j] = i ;
-                    continue;
-                }
-                if(i == 0){
-                    dp[i][j] = j ;
-                    continue;
-                }
+        vector<int> prev(word2.size() + 1 , 0);
+        vector<int> curr(word2.size() + 1 , 0);
+        for(int j = 0 ; j <= word2.size() ; j++) prev[j] = j;
+        for(int i = 1 ; i <= word1.size() ; i++){
+                curr[0] = i;
+            for(int j = 1 ; j <= word2.size() ; j++){
                 if(word1[i - 1] == word2[j - 1]){
-                    dp[i][j] = 0 + dp[i - 1][j - 1]; 
+                    curr[j] =  prev[j - 1]; 
                 }else{
-                    dp[i][j] = min(1 + dp[i][j - 1] , min(1 + dp[i - 1][j], 1 + dp[i - 1][j - 1]));
+                    curr[j] = min(1 + prev[j - 1] , min(1 + prev[j], 1 + curr[j - 1]));
                 }
             }
+            prev = curr;
         }
-        return dp[word1.size()][word2.size()];
+        return prev[word2.size()];
     }
 };
