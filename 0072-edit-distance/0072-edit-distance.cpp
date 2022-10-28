4e0eb7b6547434e9fd77e5e3b519d0cd95b2f8ab
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-28 19:05:51 +0530

    Time: 25 ms (53.56%), Space: 9.1 MB (23.66%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,17 +1,18 @@
 class Solution {
 public:
     int rec(string &word1 , string &word2 , int i , int j , vector<vector<int>>& dp){
-        if(j < 0) return i + 1;
-        if(i < 0) return j + 1;
+        if(j == 0) return i ;
+        if(i == 0) return j ;
         if(dp[i][j] != -1) return dp[i][j];
-        if(word1[i] == word2[j]){
+        if(word1[i - 1] == word2[j - 1]){
             return dp[i][j] = 0 + rec(word1 , word2 , i - 1 , j - 1 ,dp);
         }else{
             return dp[i][j] = min(1 + rec(word1 , word2 , i , j - 1 , dp) , min(1 + rec(word1 , word2 , i - 1 , j ,dp) ,1 + rec(word1 , word2 , i - 1 , j - 1 , dp)));
         }
     }
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size() , vector<int>(word2.size() , -1));
-        return rec(word1 , word2 , word1.size() - 1 , word2.size() - 1 , dp);
+        // doing index shifting for tabulation
+        vector<vector<int>> dp(word1.size() + 1 , vector<int>(word2.size() + 1 , -1));
+        return rec(word1 , word2 , word1.size() , word2.size() , dp);
     }
 };
