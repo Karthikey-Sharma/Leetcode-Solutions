c4bf1984174e879044526d7831a394c4224da561
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2022-10-28 19:03:48 +0530

    Added solution - LeetHub

---
+++ Edit Distance - GFG/edit-distance.cpp
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+  int rec(string &word1 , string &word2 , int i , int j , vector<vector<int>>& dp){
+        if(j < 0) return i + 1;
+        if(i < 0) return j + 1;
+        if(dp[i][j] != -1) return dp[i][j];
+        if(word1[i] == word2[j]){
+            return dp[i][j] = 0 + rec(word1 , word2 , i - 1 , j - 1 ,dp);
+        }else{
+            return dp[i][j] = min(1 + rec(word1 , word2 , i , j - 1 , dp) , min(1 + rec(word1 , word2 , i - 1 , j ,dp) ,1 + rec(word1 , word2 , i - 1 , j - 1 , dp)));
+        }
+    }
+    int editDistance(string word1, string word2) {
+        vector<vector<int>> dp(word1.size() , vector<int>(word2.size() , -1));
+        return rec(word1 , word2 , word1.size() - 1 , word2.size() - 1 , dp);
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
