401541bb8bbd08b39fe5888dcb7bd8991520d763
Karthikey Sharma <94706393+Karthikey-Sharma@users.noreply.github.com>
2023-01-03 06:54:40 +0530

    Added solution - LeetHub

--- Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
+++ Longest Palindromic Subsequence - GFG/longest-palindromic-subsequence.cpp
@@ -11,14 +11,14 @@ using namespace std;
 class Solution{
   public:
     int rec(string A , string B , int i , int j , vector<vector<int>>&dp){
-        if(i < 0 || j < 0) return 0;
+        if(i == 0 || j == 0) return 0;
         if(dp[i][j] != -1) return dp[i][j];
-        if(A[i] == B[j]) return dp[i][j] = 1 + rec(A , B , i - 1 , j - 1 ,dp);
+        if(A[i - 1] == B[j - 1]) return dp[i][j] = 1 + rec(A , B , i - 1 , j - 1 ,dp);
         else return dp[i][j] = max(rec(A , B , i - 1 , j , dp) , rec(A , B , i , j - 1 , dp));
     }
     int lcs(string A , string B){
-        vector<vector<int>> dp(A.size() , vector<int>(B.size() , -1));
-        return rec(A , B, A.size() - 1 , B.size() - 1 , dp);
+        vector<vector<int>> dp(A.size() + 1 , vector<int>(B.size() + 1 , -1));
+        return rec(A , B, A.size()  , B.size() , dp);
     }
     int longestPalinSubseq(string A) {
         string B = A;
